b2dcfe917192226a1273a24dd30f8874c4e6aab3
Shine SB <shine_sb@yahoo.com>
2023-08-29 11:07:58 +0630

    first Commit

---
+++ style.css
@@ -0,0 +1,52 @@
+Gui 					(	
+	Window
+
+CLi					(command line interface)
+	Linux(100) Ubuntu Cli
+
+	MAC Gui / Cli 
+
+
+Server 
+	Computer
+		Gui/CLi
+
+
+php --version
+
+	mysql --version
+
+	wampp php, mysql
+
+
+python --version
+
+
+
+
+
+Note 
+
+cd.. (back)
+
+dir (directory)
+
+cd FolderName
+
+****---****
+
+mkdir FolderName (make directory )
+
+rmdir FolderName (remove directory)
+
+
+Windows Clli / Linux Cli
+
+Moduele 2
+	C++
+	Python
+		1_sequence
+			1_hello_world.py
+
+
+s
