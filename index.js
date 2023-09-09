f285eccea6d9285b450791bcbab2f31687757599
Shine SB <143066471+shinesbglobal@users.noreply.github.com>
2023-09-10 00:52:29 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
-Gui 					(	
-	Window
+var startTime;
+var elapsedTime = 0;
+var timerInterval;
+var isRunning = false;
 
-CLi					(command line interface)
-	Linux(100) Ubuntu Cli
+function formatTime(milliseconds) {
+	var hours = Math.floor(milliseconds / 3600000);
+	var minutes = Math.floor((milliseconds % 3600000) / 60000);
+	var seconds = Math.floor((milliseconds % 60000) / 1000);
+	var milliseconds = Math.floor(milliseconds % 1000);
 
-	MAC Gui / Cli 
+	return (
+		(hours < 10 ? '0' + hours : hours) +
+		':' +
+		(minutes < 10 ? '0' + minutes : minutes) +
+		':' +
+		(seconds < 10 ? '0' + seconds : seconds)
+	);
+}
 
+function startTimer() {
+	if (!isRunning) {
+		if (elapsedTime === 0) {
+			startTime = Date.now();
+		} else {
+			startTime = Date.now() - elapsedTime;
+		}
 
-Server 
-	Computer
-		Gui/CLi
+		timerInterval = setInterval(function () {
+			var currentTime = Date.now();
+			elapsedTime = currentTime - startTime;
+			display.innerText = formatTime(elapsedTime);
+			milliseconds = elapsedTime % 1000;
+			millisecondsText.innerText = milliseconds;
+			if (milliseconds === 0) {
+				millisecondsText.innerText = '000';
+			}
+		}, 1);
 
+		isRunning = true;
+		startBtn.innerText = 'Pause';
+	} else {
+		clearInterval(timerInterval);
+		isRunning = false;
+		startBtn.innerText = 'Continue';
+	}
+}
 
-php --version
+function clearTimer() {
+	clearInterval(timerInterval);
+	elapsedTime = 0;
+	display.innerText = formatTime(elapsedTime);
+	millisecondsText.innerText = '000';
+	isRunning = false;
+	startBtn.innerText = 'Start';
+}
 
-	mysql --version
-
-	wampp php, mysql
-
-
-python --version
-
-
-
-
-
-Note 
-
-cd.. (back)
-
-dir (directory)
-
-cd FolderName
-
-****---****
-
-mkdir FolderName (make directory )
-
-rmdir FolderName (remove directory)
-
-
-Windows Clli / Linux Cli
-
-Moduele 2
-	C++
-	Python
-		1_sequence
-			1_hello_world.py
-
-
-s
